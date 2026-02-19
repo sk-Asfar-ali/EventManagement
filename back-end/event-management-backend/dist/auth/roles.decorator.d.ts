8ade5fd20c5c964c30999b5c4f7dccaade26b385
YukeshS <sy3422658@gmail.com>
2026-02-19 15:24:24 +0530

    Status: Removed dist folder

--- back-end/event-management-backend/dist/auth/roles.decorator.d.ts
+++
@@ -1,3 +0,0 @@
-import { Role } from 'src/users/users.entity';
-export declare const ROLES_KEY = "roles";
-export declare const Roles: (...roles: Role[]) => import("@nestjs/common").CustomDecorator<string>;
--- back-end/event-management-backend/dist/users/users.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './users.entity';
-export declare class UsersService {
-    private readonly userRepo;
-    constructor(userRepo: Repository<User>);
-    create(data: Partial<User>): Promise<User>;
-    findByEmail(email: string): Promise<User | null>;
-    findById(id: number): Promise<User | null>;
-}
