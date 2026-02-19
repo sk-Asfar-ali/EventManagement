c00ca3b8e6ef202e9ff2fbb0a5f29f1f0ce80952
YukeshS <sy3422658@gmail.com>
2026-02-19 15:24:24 +0530

    Status: Removed dist folder

--- back-end/event-management-backend/dist/attendance/attendance.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../users/users.entity';
-import { Event } from '../events/events.entity';
-export declare class Attendance {
-    id: number;
-    user: User;
-    event: Event;
-    isPresent: boolean;
-}
--- back-end/event-management-backend/dist/auth/auth.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthService } from './auth.service';
-export declare class AuthController {
-    private authService;
-    constructor(authService: AuthService);
-    register(body: any): Promise<import("../users/users.entity").User>;
-    login(body: any): Promise<{
-        access_token: string;
-    }>;
-}
