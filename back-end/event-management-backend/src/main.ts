b15fe0ab15ecf7c74732745ae9ab251fc3232af9
Yukesh <sy3422658@gmail.com~>
2026-03-09 21:24:48 +0530

    added Swagger

--- back-end/event-management-backend/src/main.ts
+++ back-end/event-management-backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,18 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
+
+   const config = new DocumentBuilder()
+    .setTitle('Event Management API')
+    .setDescription('API documentation for Event Management System')
+    .setVersion('1.0')
+    .addBearerAuth() // for JWT auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
