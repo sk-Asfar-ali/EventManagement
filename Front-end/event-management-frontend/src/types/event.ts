c70e8733dac4bc3e58e0d80ecb1c56a1b263148e
Asfar <Asfar@jman.com>
2026-03-09 14:40:50 +0530

    chore: completely remove Front-end folder from repository

--- Front-end/event-management-frontend/src/types/event.ts
+++
@@ -1,8 +0,0 @@
-export interface EventDto {
-  id: number;
-  title: string;
-  description: string;
-  eventDate: string;
-  status: "REGISTERED" | "NOT_REGISTERED" | "CLOSED";
-  canCancel: boolean;
-}
